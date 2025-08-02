389b50ddb211f81cbeea263f9d6654cbe46d7d5d
xr0ku <moonlightbtb@gmail.com>
2025-08-02 15:51:29 +0900

    feat: add todos api

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,11 +1,60 @@
 import express from "express";
+import cors from "cors";
+import z from "zod";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (_, res) => {
-  res.send("Hello World!");
+const prisma = new PrismaClient();
+
+app.use(cors());
+app.use(express.json());
+
+const TodoCreateSchema = z.object({
+  title: z.string().min(1, "Title is required."),
+});
+
+app.get("/health", (_, res) => {
+  res.send({ status: "ok" });
+});
+
+app.get("/todos", async (_, res) => {
+  const todos = await prisma.todo.findMany({
+    orderBy: { id: "asc" },
+  });
+
+  res.json(todos);
+});
+
+app.post("/todos", async (req, res) => {
+  const parseResult = TodoCreateSchema.safeParse(req.body);
+  if (!parseResult.success) {
+    return res.status(400).json({
+      error: parseResult.error.format(),
+    });
+  }
+
+  const newTodo = await prisma.todo.create({
+    data: { title: parseResult.data.title },
+  });
+
+  res.status(201).json(newTodo);
+});
+
+app.delete("/todos/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  if (isNaN(id)) return res.status(400).json({ error: "Invalid ID" });
+
+  const deleted = await prisma.todo
+    .delete({
+      where: { id },
+    })
+    .catch(() => null);
+
+  if (!deleted) return res.status(404).json({ error: "Todos not found" });
+
+  res.status(204).json(`✅ Successfully deleted ${id}`);
 });
 
 app.listen(PORT, () => {
