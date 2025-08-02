ac3d0bfe3827b8bfb6535c7bd7d379494b2fd2f9
xr0ku <moonlightbtb@gmail.com>
2025-08-03 07:17:10 +0900

    feat: add initila UI

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,10 +1,73 @@
-import "./App.css";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL || "http//:localhost:3000";
+
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+  createdAt: string;
+};
 
 function App() {
+  const queryClinet = useQueryClient();
+  const [title, setTitle] = useState("");
+
+  const { data: todos, isLoading } = useQuery<Todo[]>({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const res = await axios.get(`${API_URL}/todos`);
+      return res.data;
+    },
+  });
+
+  const createTodo = useMutation({
+    mutationFn: (title: string) => {
+      return axios.post(`${API_URL}/todos`, { title });
+    },
+    onSuccess: () => {
+      queryClinet.invalidateQueries({ queryKey: ["todos"] });
+      setTitle("");
+    },
+  });
+
+  const deleteTodo = useMutation({
+    mutationFn: (id: number) => axios.delete(`${API_URL}/todos/${id}`),
+    onSuccess: () => {
+      queryClinet.invalidateQueries({ queryKey: ["todos"] });
+    },
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+
   return (
-    <>
-      <p>Click on the Vite and React logos to learn more</p>
-    </>
+    <div style={{ padding: 30 }}>
+      <h1>Todo App</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (!title.trim()) return;
+          createTodo.mutate(title);
+        }}
+      >
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Enter todo title"
+        />
+      </form>
+
+      <ul>
+        {todos?.map((todo) => (
+          <li key={todo.id}>
+            {todo.title}
+            <button onClick={() => deleteTodo.mutate(todo.id)}>X</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <p>Hello world!</p>
-  </StrictMode>,
-)
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
